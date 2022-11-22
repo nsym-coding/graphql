ab350ac1b3147f734e4f9e662df3e84e8ae219e0
nsym-coding <nsym.coding@outlook.com>
2022-11-22 14:23:57 +0000

    removed dropdown selectors and labels, now just a graph section with graph by itself + related style changes

--- createGraphs.js
+++ createGraphs.js
@@ -2,8 +2,8 @@ function createXpGraph(totalXpData) {
   let graphDiv = document.getElementById("graphs");
   let xpArray = totalXpData.data.user[0].transactions;
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("height", `${100}%`);
-  svg.setAttribute("width", `${100}%`);
+  svg.setAttribute("height", `${70}%`);
+  svg.setAttribute("width", `${50}%`);
   svg.setAttribute("viewBox", `0 0 500 500`);
   let yAxis = 0;
   xpArray.forEach((elem) => {
@@ -23,10 +23,15 @@ function createXpGraph(totalXpData) {
       bar.setAttribute("width", `${1000 / xpArray.length}px`);
       bar.setAttribute("height", `${(elem.amount / 1000) * 4}`);
       bar.innerHTML = `${elem.object.name}`;
-      labelText.innerHTML = `${elem.object.name}`;
+
+      console.log("checking name is working --> ", elem.object.name);
+      labelText.innerText = `${elem.object.name}`;
+      labelText.setAttribute("x", `${0}`);
+      labelText.setAttribute("y", `${10}`);
+      labelText.setAttribute("dx", `${0.35}em`);
 
       console.log(elem.amount);
-      bar.appendChild(labelText);
+
       g.appendChild(bar);
       g.appendChild(labelText);
       svg.appendChild(g);
